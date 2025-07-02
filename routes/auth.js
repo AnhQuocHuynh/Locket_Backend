318f488ac3a76c117f4f9e0a29dc93f142d993a1
AnhQuocHuynh <quocpro12334@gmail.com>
2025-07-03 00:53:32 +0700

    feat(auth): include emailVerified in register/login responses and update tests

--- routes/auth.js
+++ routes/auth.js
@@ -74,7 +74,8 @@ router.post('/register', validateRegister, async (req, res) => {
                 id: user._id,
                 username: user.username,
                 email: user.email,
-                profilePicture: user.profilePicture
+                profilePicture: user.profilePicture,
+                emailVerified: user.emailVerified
             }
         };
 
@@ -152,7 +153,8 @@ router.post('/login', validateLogin, async (req, res) => {
                 id: user._id,
                 username: user.username,
                 email: user.email,
-                profilePicture: user.profilePicture
+                profilePicture: user.profilePicture,
+                emailVerified: user.emailVerified
             }
         });
     } catch (error) {
